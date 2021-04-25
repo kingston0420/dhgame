27ed61672710c5952df0dc1dcaedc735095b4a39
Kingston <kingston880420@gmail.com>
2021-04-24 19:36:10 -0700

    deploy

---
+++ precache-manifest.fdb5db091813038aac42732cac6f49d4.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c3eac18f5fd44343310",
+    "url": "/dhgame/css/about.90dd9656.css"
+  },
+  {
+    "revision": "9c3eac18f5fd44343310",
+    "url": "/dhgame/css/about.90dd9656.css.map"
+  },
+  {
+    "revision": "299e46836fdd942f6b4b",
+    "url": "/dhgame/css/app.9cf7ac71.css"
+  },
+  {
+    "revision": "299e46836fdd942f6b4b",
+    "url": "/dhgame/css/app.9cf7ac71.css.map"
+  },
+  {
+    "revision": "eb389900c9091e7d71b338730400e929",
+    "url": "/dhgame/favicon.ico"
+  },
+  {
+    "revision": "8ba628eaa0d936a34402d5e0d6c132df",
+    "url": "/dhgame/img/DH.8ba628ea.png"
+  },
+  {
+    "revision": "13b04e9aaaa5bb23288df46902fe2cd7",
+    "url": "/dhgame/img/blue_white.13b04e9a.png"
+  },
+  {
+    "revision": "6143db3b90accbb5ac3dddf21663419f",
+    "url": "/dhgame/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "62b4784cd52cabee9ca632b0394b7556",
+    "url": "/dhgame/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "6143db3b90accbb5ac3dddf21663419f",
+    "url": "/dhgame/img/icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "62b4784cd52cabee9ca632b0394b7556",
+    "url": "/dhgame/img/icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "85e60a5887e7acc724cfda13b5343f1e",
+    "url": "/dhgame/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "24987da7df5e156c169a576058975f48",
+    "url": "/dhgame/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "af99013a48df0f28c76b233bd7a20c0a",
+    "url": "/dhgame/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "727dc8400614b2a07a6dacd6c9221e0c",
+    "url": "/dhgame/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "65425d92093631a13022f998b7f8db75",
+    "url": "/dhgame/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "af99013a48df0f28c76b233bd7a20c0a",
+    "url": "/dhgame/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "89c58fdff224b4aa9e07c1506792f486",
+    "url": "/dhgame/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "7b41b6250c16266e30572203acd36c76",
+    "url": "/dhgame/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "eb389900c9091e7d71b338730400e929",
+    "url": "/dhgame/img/icons/favicon.ico"
+  },
+  {
+    "revision": "ca9b72d123a80d2915c1292b975dfec1",
+    "url": "/dhgame/img/icons/manifest.json"
+  },
+  {
+    "revision": "58c5a9f7327d75a575c8b486b40dd7cd",
+    "url": "/dhgame/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "df80734255051f952db4bc5642a2c4be",
+    "url": "/dhgame/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "1082015576ca7b59fe01521a838da385",
+    "url": "/dhgame/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "151f4ad63f0d9eddcd00c368493eb80a",
+    "url": "/dhgame/index.html"
+  },
+  {
+    "revision": "9c3eac18f5fd44343310",
+    "url": "/dhgame/js/about.9ab8d278.js"
+  },
+  {
+    "revision": "9c3eac18f5fd44343310",
+    "url": "/dhgame/js/about.9ab8d278.js.map"
+  },
+  {
+    "revision": "299e46836fdd942f6b4b",
+    "url": "/dhgame/js/app.13e2c4c0.js"
+  },
+  {
+    "revision": "299e46836fdd942f6b4b",
+    "url": "/dhgame/js/app.13e2c4c0.js.map"
+  },
+  {
+    "revision": "49b7c2730b3ae786cf79",
+    "url": "/dhgame/js/chunk-vendors.aa8c4628.js"
+  },
+  {
+    "revision": "49b7c2730b3ae786cf79",
+    "url": "/dhgame/js/chunk-vendors.aa8c4628.js.map"
+  },
+  {
+    "revision": "2aa5fefd85d8ac58c6c2f60bcc5ff7b4",
+    "url": "/dhgame/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dhgame/robots.txt"
+  }
+]);
